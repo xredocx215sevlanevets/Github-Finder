8adbc8f685041cfa9e4ee881eaaf4993b13dcb04
xredocx215sevlanevets <stevenalves512@gmail.com>
2020-02-17 11:28:50 -0100

    spinner e users props

--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -1,17 +1,40 @@
-import React, { Component } from 'react';
+import React from 'react';
 import UserItem from './UserItem';
 
-class Users extends Component {
-  render() {
+import Spinner from '../layout/Spinner';
+
+import PropTypes from 'prop-types';
+
+const Users = ({ users, loading }) => {
+  if (loading) {
+    return <Spinner />;
+  } else {
     return (
       <div className="teste" style={userStyle}>
-        {this.props.users.map(user => (
+        {users.map(user => (
           <UserItem key={user.id} user={user} />
         ))}
       </div>
     );
   }
-}
+
+  // {
+  //   loading ? (
+  //     <Spinner />
+  //   ) : (
+  //     <div className="teste" style={userStyle}>
+  //       {users.map(user => (
+  //         <UserItem key={user.id} user={user} />
+  //       ))}
+  //     </div>
+  //   );
+  // }
+};
+
+Users.PropTypes = {
+  users: PropTypes.array.isRequired,
+  loading: PropTypes.bool.isRequired
+};
 
 const userStyle = {
   display: 'grid',
