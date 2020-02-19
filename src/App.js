773f66d0a22c0b218afef3ceb4e11b298c7cf025
xredocx215sevlanevets <stevenalves512@gmail.com>
2020-02-19 12:45:13 -0100

    setup routing

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import axios from 'axios';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 import './App.css';
 
 import Navbar from './components/layout/Navbar';
-import Users from './components/users/Users';
-
-import Search from './components/users/Search';
 import Alert from './components/layout/Alert';
+
+import Users from './components/users/Users';
+import Search from './components/users/Search';
+
+import About from './components/pages/About';
 
 class App extends Component {
   state = {
@@ -32,25 +35,40 @@ class App extends Component {
 
   setAlert = (msg, type) => {
     this.setState({ alert: { msg, type } });
-
     setTimeout(() => this.setState({ alert: null }), 5000);
   };
 
   render() {
     return (
-      <div className="App">
-        <Navbar />
-        <div className="container">
-          <Alert alert={this.state.alert} />
-          <Search
-            searchUsers={this.searchUsers}
-            clearUsers={this.clearUsers}
-            showClear={this.state.users.length > 0 ? true : false}
-            setAlert={this.setAlert}
-          />
-          <Users loading={this.state.loading} users={this.state.users} />
+      <Router>
+        <div className="App">
+          <Navbar />
+          <div className="container">
+            <Alert alert={this.state.alert} />
+            <Switch>
+              <Route
+                exact
+                path="/"
+                render={props => (
+                  <Fragment>
+                    <Search
+                      searchUsers={this.searchUsers}
+                      clearUsers={this.clearUsers}
+                      showClear={this.state.users.length > 0 ? true : false}
+                      setAlert={this.setAlert}
+                    />
+                    <Users
+                      loading={this.state.loading}
+                      users={this.state.users}
+                    />
+                  </Fragment>
+                )}
+              />
+              <Route exact path="/about" component={About} />
+            </Switch>
+          </div>
         </div>
-      </div>
+      </Router>
     );
   }
 }
---
+++ src/components/pages/About.js
@@ -0,0 +1,11 @@
+import React, { Fragment } from 'react';
+
+export default function pages() {
+  return (
+    <Fragment>
+      <h1>About this App</h1>
+      <p>App to search Github users</p>
+      <p>Version 1.0.0</p>
+    </Fragment>
+  );
+}
