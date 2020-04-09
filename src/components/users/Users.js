c52a131ed8d37139915a9c8ab3fc7fe85344cabd
xredocx215sevlanevets <stevenalves512@gmail.com>
2020-04-09 18:14:55 -0100

    refatoring

--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -6,40 +6,28 @@ import Spinner from '../layout/Spinner';
 import PropTypes from 'prop-types';
 
 const Users = ({ users, loading }) => {
+  Users.propTypes = {
+    users: PropTypes.array.isRequired,
+    loading: PropTypes.bool.isRequired,
+  };
+
+  const userStyle = {
+    display: 'grid',
+    gridTemplateColumns: 'repeat(3, 1fr)',
+    gridGap: '1rem',
+  };
+
   if (loading) {
     return <Spinner />;
   } else {
     return (
       <div className="teste" style={userStyle}>
-        {users.map(user => (
+        {users.map((user) => (
           <UserItem key={user.id} user={user} />
         ))}
       </div>
     );
   }
-
-  // {
-  //   loading ? (
-  //     <Spinner />
-  //   ) : (
-  //     <div className="teste" style={userStyle}>
-  //       {users.map(user => (
-  //         <UserItem key={user.id} user={user} />
-  //       ))}
-  //     </div>
-  //   );
-  // }
-};
-
-Users.propTypes = {
-  users: PropTypes.array.isRequired,
-  loading: PropTypes.bool.isRequired
-};
-
-const userStyle = {
-  display: 'grid',
-  gridTemplateColumns: 'repeat(3, 1fr)',
-  gridGap: '1rem'
 };
 
 export default Users;
